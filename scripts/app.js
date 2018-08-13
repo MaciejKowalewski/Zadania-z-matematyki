efbf67a0b87a7603aa5167ac049107e755b0a739
Maciej Kowalewski <maciejkowalewski95@gmail.com>
2018-08-13 21:06:31 +0200

    to chyba działa :O

--- scripts/app.js
+++ scripts/app.js
@@ -2,9 +2,17 @@ var app = new Vue({
     el: '#app',
     data: {
         schlClasses: [
-            {link: 'IV A'},
-            {link: 'VI A'},
-            {link: 'VIII A'},
+            {id: 4, link: 'Zadania dla IV klasy'},
+            {id: 5, link: 'Zadania dla V klasy'},
+            {id: 6, link: 'Zadania dla VI klasy'},
+            {id: 7, link: 'Zadania dla VII klasy'},
+            {id: 8, link: 'Zadania dla VIII klasy'},
+            {id: 9, link: 'Zadania dla IX klasy'},
         ],
+    },
+    methods: {
+        showMathChapters: function(id){
+            console.log(id)
+        }
     },
 })
