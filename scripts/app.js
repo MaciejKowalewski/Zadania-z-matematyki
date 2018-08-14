8b2d9756ea530e58a0348ef0f57584129546f916
Maciej Kowalewski <maciejkowalewski95@gmail.com>
2018-08-14 17:16:07 +0200

    Dodano 1 rozdział do 4 klasy

--- scripts/app.js
+++ scripts/app.js
@@ -2,27 +2,28 @@ var app = new Vue({
     el: '#app',
     data: {
         schlClasses: [
-            {id: 4, link: 'Zadania dla IV klasy'},
-            {id: 5, link: 'Zadania dla V klasy'},
-            {id: 6, link: 'Zadania dla VI klasy'},
-            {id: 7, link: 'Zadania dla VII klasy'},
-            {id: 8, link: 'Zadania dla VIII klasy'},
-            {id: 9, link: 'Zadania dla IX klasy'},
+            {id: 0, link: 'Zadania dla IV klasy'},
+            {id: 1, link: 'Zadania dla V klasy'},
+            {id: 2, link: 'Zadania dla VI klasy'},
+            {id: 3, link: 'Zadania dla VII klasy'},
+            {id: 4, link: 'Zadania dla VIII klasy'},
+            {id: 5, link: 'Zadania dla IX klasy'},
         ],
         chaptersForLvl4: [
-            {chapter: 'Rachunki pamięciowe – dodawanie i odejmowanie'},
+            {chapter1: 'Rachunki pamięciowe – dodawanie i odejmowanie'},
         ],
     },
     methods: {
         exitChapters: function(){
-            let chapterSection = document.querySelector('.chapter-section');
-            chapterSection.classList.remove(chapterSection.classList[2]);
+            let chapterSection = document.querySelector('.chapter-section-visible');
             chapterSection.classList.remove('chapter-section-visible');
         },
         showMathChapters: function(id){
-            let chapterSection = document.querySelector('.chapter-section');
-            chapterSection.classList.add('chapter-section-visible');
-            chapterSection.classList.add('class'+id);
+            let chapterSection = document.querySelectorAll('.chapter-section');
+            chapterSection[id].classList.add('chapter-section-visible');
+        },
+        showChapter: function(number){
+          console.log("chapter number "+number);  
         },
     },
 })
